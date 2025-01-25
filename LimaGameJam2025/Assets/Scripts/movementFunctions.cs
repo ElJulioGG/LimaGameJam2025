00f9b6a555bd3cbba32b2e094082181cf495b407
DaCrIaX <moxgzzttdev@gmail.com>
2025-01-25 02:46:29 -0500

    Camara del jugador
    Importado el paquete de cinemachine y seteado para controlar la cámara del jugador por medio del input system

--- LimaGameJam2025/Assets/Scripts/movementFunctions.cs
+++ LimaGameJam2025/Assets/Scripts/movementFunctions.cs
@@ -6,9 +6,15 @@ public class movementFunctions : MonoBehaviour
 {
     private CharacterController playerController;
     private Vector3 playerVelocity;
+    
 
     [Header ("Movement Parameters")]
     [SerializeField, Range(0f, 20f)] private float playerSpeed;
+    
+    [Header ("Jump Parameters")]
+    [SerializeField] private float worldGravity = -9.8f;
+    [SerializeField] private bool isGrounded;
+
 
     void Start()
     {
@@ -17,7 +23,7 @@ public class movementFunctions : MonoBehaviour
 
     void Update()
     {
-        
+        isGrounded = playerController.isGrounded;
     }
 
     public void ProcessMove(Vector2 input)
@@ -26,5 +32,13 @@ public class movementFunctions : MonoBehaviour
         moveDir.x = input.x;
         moveDir.z = input.y;
         playerController.Move(transform.TransformDirection(moveDir) * playerSpeed * Time.deltaTime);
+
+        playerVelocity.y += worldGravity * Time.deltaTime;
+
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+
+        playerController.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
     }
 }
