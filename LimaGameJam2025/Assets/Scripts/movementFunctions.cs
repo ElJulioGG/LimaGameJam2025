58ef226d28aa8afb47e034ec1f78b8662eb7b63d
MapleSyrup63 <rodrigo.ramirez.2005@gmail.com>
2025-01-25 11:48:24 -0500

    Added a coyote timer, only took me the whole damn night!

--- LimaGameJam2025/Assets/Scripts/movementFunctions.cs
+++ LimaGameJam2025/Assets/Scripts/movementFunctions.cs
@@ -4,29 +4,58 @@ using UnityEngine;
 
 public class movementFunctions : MonoBehaviour
 {
-    private lookFunctions _look;
+    
+    public lookFunctions _look;
     private CharacterController playerController;
     private Vector3 playerVelocity;
     
 
     [Header ("Movement Parameters")]
-    [SerializeField, Range(0f, 20f)] private float playerSpeed;
+    [SerializeField, Range(0f, 20f)] public float playerSpeed;
     
     [Header ("Jump Parameters")]
     [SerializeField] private float worldGravity = -9.8f;
     [SerializeField] private bool isGrounded;
     [SerializeField, Range(0f, 20f)] private float jumpHeight;
-
+    [SerializeField] private float coyoteTime = 1.5f;
+    [SerializeField] private float coyoteTimeCounter;
+    [SerializeField] private bool canJump = false;
+    private float targetJumpVelocity = 0.0f;
 
     void Start()
     {
         _look = GetComponent<lookFunctions>();
         playerController = GetComponent<CharacterController>();
+        targetJumpVelocity = Mathf.Sqrt(jumpHeight * -3.0f * worldGravity);
     }
 
     void Update()
     {
         isGrounded = playerController.isGrounded;
+
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+
+            //Bandaid fix for the ghost double-jump bug
+            if (playerVelocity.y > 0){
+                canJump = false;
+            }
+            else{
+                canJump = true;
+            }
+
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+
+            if (coyoteTimeCounter < 0)
+            {
+                coyoteTimeCounter = 0;
+                canJump = false;
+            }
+        }
     }
 
     public void ProcessMove(Vector2 input)
@@ -47,9 +76,20 @@ public class movementFunctions : MonoBehaviour
     }
 
     public void Jump(){
+        //Incomplete maybe, if any jump-related bugs arise, check this "if" conditional first
         if (isGrounded)
+        {   
+            playerVelocity.y = targetJumpVelocity;
+            canJump = false;
+        }
+        else
         {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * worldGravity);
+            if (coyoteTimeCounter > 0 && canJump)
+            {
+                playerVelocity.y = targetJumpVelocity;
+                coyoteTimeCounter = 0;
+                canJump = false;
+            }
         }
     }
 }
